4f3ad356b86ba02e208f3cdbf14fb4210bba4cc0
FireMario211 <firemario07@gmail.com>
2018-04-15 09:36:13 -0400

    reee

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+
+    CROSS SERVER MULTIPLAYER
+            V1.0.0
+        FireMario211
+
+
+License: MIT
+*/
+
+
+
+const { Client, RichEmbed } = require('discord.js')
+const bot = new Client()
